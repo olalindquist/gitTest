9aba8131279826418e1b0f993c3ba87ff89d6f9a
olalindquist <ola_lindquist@hotmail.com>
2020-09-20 14:21:16 +0200

    added a line

--- HelloWord.java
+++ HelloWord.java
@@ -1,6 +1,7 @@
 public class HelloWord{
     public static void main(String args[]){
-	System.out.println("Hello world!!");
+	System.out.println("Hello world!! ");
+	System.out.println("Line 2 added! " );
 	
     }
 }
